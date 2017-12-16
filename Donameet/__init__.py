87eb02e66cf596408b8b26c3484759046a31eb45
Wira Abdillah S <wirabdillah@gmail.com>
2017-12-16 21:41:15 +0700

    Add authenticaton and basic page

--- Donameet/__init__.py
+++ Donameet/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 from flask_migrate import Migrate
 from Donameet.views import views
+from Donameet.auth import auth
+
+import os
 
 
 def create_app():
     app = Flask(__name__)
     with app.app_context():
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}/app.db'.format(os.path.dirname(os.path.abspath(__file__)))
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SESSION_TYPE'] = 'filesystem'
 
         from Donameet.models import db, User, Request, ForeignContact, ContactLog
 
@@ -16,6 +21,10 @@ def create_app():
         migrate = Migrate()
         migrate.init_app(app, db)
 
+        session = Session()
+        session.init_app(app)
+
         app.register_blueprint(views)
+        app.register_blueprint(auth)
 
     return app
