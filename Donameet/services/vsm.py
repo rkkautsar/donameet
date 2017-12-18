27c99377835913c80a6ecbb3a281bb467f433c12
Wira Abdillah S <wirabdillah@gmail.com>
2017-12-19 02:15:35 +0700

    Add missing distance for calculating donor norm

--- Donameet/services/vsm.py
+++ Donameet/services/vsm.py
@@ -69,12 +69,12 @@ class VSM:
                 dot += patient[i] * user[i]
                 donor_norm += user[i] * user[i]
 
-            donor_norm = donor_norm ** 0.5
-
             donor_pos = (user[7], user[8])
             if (user[7] != 0 or user[8] != 0) and (patient[7] != 0 or patient[8] != 0):
                 dot += 10 / (1 + vincenty(patient_pos, donor_pos).kilometers**2)
+                donor_norm += 10 / (1 + vincenty(patient_pos, donor_pos).kilometers**2)
 
+            donor_norm = donor_norm ** 0.5
             cosine_similarity = dot / (patient_norm * donor_norm)
 
             vectors.append({'username': user[0], 'similarity': cosine_similarity})
