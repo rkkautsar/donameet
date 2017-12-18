b36b6101410cbfbd130f563f9335d3c4810193f7
Wira Abdillah S <wirabdillah@gmail.com>
2017-12-18 21:40:25 +0700

    Fix corner case again

--- Donameet/services/vsm.py
+++ Donameet/services/vsm.py
@@ -72,7 +72,7 @@ class VSM:
                 donor_norm = donor_norm ** 0.5
 
                 donor_pos = (user[7], user[8])
-                if user[7] != 0 or user[8] != 0:
+                if (user[7] != 0 or user[8] != 0) and (patient[7] != 0 or patient[8] != 0):
                     dot += 10 / (1 + vincenty(patient_pos, donor_pos)**2)
 
                 cosine_similarity = dot / (patient_norm * donor_norm)
