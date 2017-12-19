2fa13d1f40e8d4e70b8df8ad785c282aa4faa59e
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2017-12-19 09:48:02 +0700

    edit vincenty

--- test_set.sh
+++ test_set.sh
@@ -21,9 +21,16 @@
 # http post :4000/add-donor username="user19" contact_phone="0" blood_type="O" rhesus="+" location="Depok" lat="-7.7689657" lng="110.405357249038" -f
 # http post :4000/add-donor username="user29" contact_phone="0" blood_type="A" rhesus="+" location="Depok Jawa Barat" lat="-6.4074244" lng="106.8140193" -f
 
-http post :4000/add-request username="patient1" contact_phone="0" blood_type="AB" rhesus="+" amount="1" location="UGM Jogja" lat="-7.77104835" lng="110.379509349734" text="asd" -f
-http post :4000/add-request username="patient2" contact_phone="0" blood_type="AB" rhesus="-" amount="1" location="UGM Jogja" lat="-7.77104835" lng="110.379509349734" text="asd" -f
-http post :4000/add-request username="patient3" contact_phone="0" blood_type="O" rhesus="+" amount="1" location="UGM Jogja" lat="-7.77104835" lng="110.379509349734" text="asd" -f
-http post :4000/add-request username="patient4" contact_phone="0" blood_type="O" rhesus="-" amount="1" location="UGM Jogja" lat="-7.77104835" lng="110.379509349734" text="asd" -f
-http post :4000/add-request username="patient5" contact_phone="0" blood_type="A" rhesus="+" amount="1" location="UGM Jogja" lat="-7.77104835" lng="110.379509349734" text="asd" -f
-http post :4000/add-request username="patient6" contact_phone="0" blood_type="B" rhesus="+" amount="1" location="UGM Jogja" lat="-7.77104835" lng="110.379509349734" text="asd" -f
+# http post :4000/add-request username="patient1" contact_phone="0" blood_type="AB" rhesus="+" amount="1" location="UGM Jogja" lat="-7.77104835" lng="110.379509349734" text="asd" -f
+# http post :4000/add-request username="patient2" contact_phone="0" blood_type="AB" rhesus="-" amount="1" location="UGM Jogja" lat="-7.77104835" lng="110.379509349734" text="asd" -f
+# http post :4000/add-request username="patient3" contact_phone="0" blood_type="O" rhesus="+" amount="1" location="UGM Jogja" lat="-7.77104835" lng="110.379509349734" text="asd" -f
+# http post :4000/add-request username="patient4" contact_phone="0" blood_type="O" rhesus="-" amount="1" location="UGM Jogja" lat="-7.77104835" lng="110.379509349734" text="asd" -f
+# http post :4000/add-request username="patient5" contact_phone="0" blood_type="A" rhesus="+" amount="1" location="UGM Jogja" lat="-7.77104835" lng="110.379509349734" text="asd" -f
+# http post :4000/add-request username="patient6" contact_phone="0" blood_type="B" rhesus="+" amount="1" location="UGM Jogja" lat="-7.77104835" lng="110.379509349734" text="asd" -f
+
+http post :4000/add-request username="patient7" contact_phone="0" blood_type="AB" rhesus="+" amount="1" location="Cilodong" lat="-6.4249935" lng="106.843683" text="asd" -f
+http post :4000/add-request username="patient8" contact_phone="0" blood_type="AB" rhesus="-" amount="1" location="Cilodong" lat="-6.4249935" lng="106.843683" text="asd" -f
+http post :4000/add-request username="patient9" contact_phone="0" blood_type="O" rhesus="+" amount="1" location="Cilodong" lat="-6.4249935" lng="106.843683" text="asd" -f
+http post :4000/add-request username="patient10" contact_phone="0" blood_type="O" rhesus="-" amount="1" location="Cilodong" lat="-6.4249935" lng="106.843683" text="asd" -f
+http post :4000/add-request username="patient11" contact_phone="0" blood_type="A" rhesus="+" amount="1" location="Cilodong" lat="-6.4249935" lng="106.843683" text="asd" -f
+http post :4000/add-request username="patient12" contact_phone="0" blood_type="B" rhesus="+" amount="1" location="Cilodong" lat="-6.4249935" lng="106.843683" text="asd" -f
